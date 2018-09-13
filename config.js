6180189fe18e721d13db92173f8dd4ce8d48475f
Max Wood <max.w29@hotmail.com>
2018-09-13 16:56:22 -0500

    removed config file from online repo

--- config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user123:user123@ds125862.mlab.com:25862/dfs-analytics-react-capstone';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://user123:user123@ds125862.mlab.com:25862/dfs-analytics-react-capstone';
-exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || "Aruba or Jamaica";
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '12h';
