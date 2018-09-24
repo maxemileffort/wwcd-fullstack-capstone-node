d0cc8c01bd5858b826e366ff5ec0da6d331afd41
Maxemillian Wood <Max W>
2018-09-24 02:20:28 -0500

    account update fixed

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const morgan        = require('morgan');
 const csv           = require('csvtojson');
 const bcrypt        = require('bcryptjs');
 const multer        = require('multer');
+const async 		= require('async');
 const jwt           = require('jsonwebtoken');
 const passport      = require('passport');
 
@@ -375,127 +376,93 @@ app.post("/message/send", (req, res)=>{
 //PUT endpoints
 //====================
 // update account info
-// app.put("/user/update", (req, res) => {
-//     let accountType = req.body.accountType; 
-//     let newEmail = req.body.accountNewEmail;
-//     let currentEmail = req.body.accountCurrentEmail;
-//     let newPassword1 = req.body.accountNewPassword1;
-//     let newPassword2 = req.body.accountNewPassword2;
-//     let currentPassword = req.body.accountCurrentPassword;
-//     let newsletter = req.body.newsletter;
-//     console.log(req.body)
-//     let updateObj = {};
-//     let _user;
-//     async.series([
-//         function(callback) {
-//             User.findOne(
-//                 {email: currentEmail}
-//             )
-//             .then(user=>{
-//                 console.log(user)
-//                 if (user === null || user === undefined){
-//                     return res.status(200).json({message: "User doesn't exist."});
-//                 }
-//                 //validate password
-//                 let hash = user.password;
-//                 bcrypt.compare(currentPassword, hash, (err, result)=>{
-//                     if (err) { // passwords don't match
-//                     return res.status(401).json({
-//                         message: "Auth failed"
-//                     });
-//                     }
-//                     if (result) { // passwords do match
-//                         //extract user info to outer function scope
-//                         _user = user
-//                         callback(null, _user);
-//                         console.log("user pre-update")
-//                         console.log(_user)
-//                     }
-//                 })
-//             })
-//             .catch(err => {
-//                 console.log(err);
-//                 res.status(500).json({ message: "Error updating your account." })
-//             })
-//         },
-//         function(callback) {
-//             // Construct update object
-//             // update account type
-//             if(_user.accountType !== accountType){ // this is always submitted, so it needs to check against stored value
-//                 console.log("Changing account type to " + accountType)
-//                 updateObj.assign(updateObj, {accountType})
-//             }
-//             // update email
-//             if(newEmail){ // this is an optional value, so just need to see if exists
-//                 console.log("Changing email to " + newEmail)
-//                 Object.assign(updateObj, {email: newEmail})
-//             }
-//             // update password
-//             if (newPassword1 && newPassword2 && newPassword1 === newPassword2){
-//                 console.log("Changing password...")
-//                 //create an encryption key
-//                 bcrypt.genSalt(10, (err, salt) => {
-//                     //if creating the key returns an error...
-//                     if (err) {
-//                         //display it
-//                         return res.status(500).json({
-//                             message: "Couldn't create salt."
-//                         });
-//                     }
-//                     //using the encryption key above generate an encrypted pasword
-//                     bcrypt.hash(newPassword1, salt, (err, hash) => {
-//                         //if creating the ncrypted pasword returns an error..
-//                         if (err) {
-//                             //display it
-//                             return res.status(500).json({
-//                                 message: "Couldn't create hash."
-//                             });
-//                         }
-//                         Object.assign(updateObj, {password: hash})
-//                     });
-//                 });
-//             }
-//             // update newsletter status
-//             if (_user.newsletter !== newsletter){
-//                 console.log("Changing newsletter mailing status to " + newsletter)
-//                 Object.assign(updateObj, {newsletter})
-//             }
-    
-//             console.log(updateObj)
-//             callback(null, updateObj);
-//         },
-//         function(callback) {
-//             User.update(
-//                 {email: currentEmail},
-//                 updateObj,
-//                 {
-//                     returnNewDocument: true
-//                 }
-//             )
-//             .then(user=> {
-//                 res.status(200).json({
-//                     message: "Update successful",
-//                     user: user
-//                 });
-//                 console.log("User post-update")
-//                 console.log(user)
-//                 callback(null, user);
-//             })
-//             .catch(err => {
-//                 console.log(err);
-//                 res.status(500).json({ message: "Error updating your account." })
-//             })
-//         }
-//     ],
-//     // optional callback
-//     function(err, results) {
-//         if(err){
-//             console.log(err)
-//             res.status(500).json({ message: "Error updating your account." })
-//         }
-//         console.log(results)
-//     });
-// });
+app.put("/user/update", (req, res) => {
+    let currentEmail = req.body.accountCurrentEmail;
+    let accountType = req.body.accountType; 
+    let newEmail = req.body.accountNewEmail;
+    let newPassword1 = req.body.accountNewPassword1;
+    let newPassword2 = req.body.accountNewPassword2;
+    let currentPassword = req.body.accountCurrentPassword;
+    let newsletter = req.body.newsletter;
+    console.log(req.body)
+    let updateObj = {
+        newsletter,
+        accountType
+    };
+    if (newEmail){
+        updateObj.email = newEmail;
+    }
+    if (newPassword1 && newPassword2 && newPassword1 === newPassword2){
+        updateObj.password = bcrypt.hashSync(newPassword1, 10);
+    }
+	console.log(updateObj)
+    let _user;
+    async.series([
+        function(callback) {
+            User.findOne(
+                {email: currentEmail}
+            )
+            .then(user=>{
+                if (user === null || user === undefined){
+                    return res.status(200).json({message: "User doesn't exist."});
+                }
+                //validate password
+                console.log('validating password...');
+                let hash = user.password;
+                bcrypt.compare(currentPassword, hash)
+                .then(result=>{
+                    if (!result){
+                        console.log('Passwords did not match')
+                        return res.status(401).json({
+                            message: "Auth failed"
+                        });
+                    } 
+                    else {
+                        console.log("Passwords match, beginning update...")
+                        _user = user
+                        callback(null, _user)
+                    }
+                })
+                .catch(err=>{
+                    console.log(err)
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                })
+            })	
+        },
+        function(callback) {
+            User.update(
+                {email: currentEmail},
+                updateObj,
+                {
+                    returnNewDocument: true
+                }
+            )
+            .then(user=> {
+                res.status(200).json({
+                    message: "Update successful",
+                    user: user
+                });
+                console.log("User post-update")
+                console.log(user)
+                callback(null, user);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ message: "Error updating your account." })
+            })
+        }
+    ],
+    // optional callback
+    function(err, results) {
+        if(err){
+            console.log(err)
+            res.status(500).json({ message: "Error updating your account." })
+        }
+        console.log(results)
+    });
+});
 
 // change lineups
 // add as a feature later
